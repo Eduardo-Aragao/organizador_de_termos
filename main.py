529889875cc89f2167ec995672db4e964c1de6f7
Eduardo Aragao Silva <eduardo.silva@tutiplast.local>
2026-04-16 17:06:57 -0300

    fix: corrigido as importações de novas funções na main e refatorado o código do menu de opção

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
-from src.organizar_termos import organizar_termos
+from src.organizar_termos import organizar_termos_ativos, organizar_termos_si
 from src.backup_termos import backup_termos
 
 
 def menu():
     while True:
         print("\n" + "=" * 40)
-        print("1. Organizar termos")
-        print("2. Fazer backup")
-        print("3. Sair")
+        print("1. Organizar termos ativos")
+        print("2. Organizar termos SI")
+        print("3. Fazer backup")
+        print("4. Sair")
         print("=" * 40)
 
         escolha = input("Escolha uma opção: ").strip()
 
         if escolha == "1":
-            organizar_termos()
+            organizar_termos_ativos()
         elif escolha == "2":
-            backup_termos()
+            organizar_termos_si()
         elif escolha == "3":
+            backup_termos()
+        elif escolha == "4":
             print("Até logo!")
             break
         else:
             print("Opção inválida. Tente novamente.")
-
 
 if __name__ == "__main__":
     menu()
