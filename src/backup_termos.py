ee460a4897c66adeb861576c87c31345d08cf772
Eduardo Aragão <eduardo.aragaosoft@outlook.com>
2026-05-21 17:44:25 -0300

    refactor: unifica o backup para todas as pastas e resolve erros de importações

--- src/backup_termos.py
+++ src/backup_termos.py
@@ -1,36 +1,30 @@
 import shutil
 from pathlib import Path
 import datetime
-from .config import pasta_destino_ativos_raiz, pasta_destino_si_raiz, pasta_backup
-
-
-def backup_termos_ativos():
-    """Realiza backup dos termos de ativos organizados."""
-    if not pasta_backup.exists():
-        pasta_backup.mkdir(parents=True, exist_ok=True)
-
-    destino_backup = pasta_backup / pasta_destino_ativos_raiz.name
-    if destino_backup.exists():
-        print(f"O backup já existe em: {destino_backup}")
-        return
-    shutil.copytree(pasta_destino_ativos_raiz, destino_backup)
-    print(f"Backup realizado em: {destino_backup}{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
-
-def backup_termos_si():
-    """Realiza backup dos termos de SI organizados."""
-    if not pasta_backup.exists():
-        pasta_backup.mkdir(parents=True, exist_ok=True)
-
-    destino_backup = pasta_backup / pasta_destino_si_raiz.name
-    if destino_backup.exists():
-        print(f"O backup já existe em: {destino_backup}")
-        return
-
-    shutil.copytree(pasta_destino_si_raiz, destino_backup)
-    print(f"Backup realizado em: {destino_backup}{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
-
+from .config import (
+    pasta_backup_equipamentos,
+    pasta_backup_notebooks,
+    pasta_backup_si,
+    pasta_destino_equipamentos,
+    pasta_destino_notebooks,
+    pasta_destino_si,
+)
 
 def backup_termos():
     """Realiza backup de todos os termos organizados."""
-    backup_termos_ativos()
-    backup_termos_si()
+    data_hora = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+
+    # Backup de equipamentos
+    backup_equipamentos = pasta_backup_equipamentos / f"backup_equipamentos_{data_hora}"
+    shutil.copytree(pasta_destino_equipamentos, backup_equipamentos)
+    print(f"Backup de equipamentos criado em: {backup_equipamentos}")
+
+    # Backup de notebooks
+    backup_notebooks = pasta_backup_notebooks / f"backup_notebooks_{data_hora}"
+    shutil.copytree(pasta_destino_notebooks, backup_notebooks)
+    print(f"Backup de notebooks criado em: {backup_notebooks}")
+
+    # Backup de SI
+    backup_si = pasta_backup_si / f"backup_si_{data_hora}"
+    shutil.copytree(pasta_destino_si, backup_si)
+    print(f"Backup de SI criado em: {backup_si}")
