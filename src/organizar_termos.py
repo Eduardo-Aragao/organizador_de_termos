c500874e6173ccda18e5e888196fabda8590e826
Eduardo Aragão <eduardo.aragaosoft@outlook.com>
2026-05-21 17:43:09 -0300

    refactor: fragmenta funções no arquivo e adiciona validação de categorias e setores

--- src/organizar_termos.py
+++ src/organizar_termos.py
@@ -1,71 +1,64 @@
 import shutil
 from pathlib import Path
-from .config import pasta_entrada, pasta_destino_ativos_raiz, pasta_invalidos_ativos, pasta_destino_si_raiz, pasta_invalidos_si
+from .config import pasta_entrada, pasta_invalidos, pasta_destino_equipamentos, pasta_destino_notebooks, pasta_destino_si, pasta_backup_equipamentos, pasta_backup_notebooks, pasta_backup_si
 
-
-def organizar_termos_ativos():
-    """Organiza os termos em pastas por setor."""
-    if not pasta_destino_ativos_raiz.exists():
-        pasta_destino_ativos_raiz.mkdir(parents=True, exist_ok=True)
-    if not pasta_invalidos_ativos.exists():
-        pasta_invalidos_ativos.mkdir(parents=True, exist_ok=True)
-
-    contador = 0
-
-    for arquivo in pasta_entrada.rglob("*.pdf"):
-        contador += 1
-        try:
-            nome_completo = arquivo.stem
-            partes = nome_completo.split('-')
-
-            if len(partes) == 3:
-                '''Categorias = VPN, Mídias Removíveis, Monitores, Periféricos, Notebooks'''
-                nome, categoria, setor = partes
-                caminho_setor = pasta_destino_ativos_raiz / setor
-
-                if not caminho_setor.exists():
-                    caminho_setor.mkdir(parents=True, exist_ok=True)
-
-                shutil.move(str(arquivo), str(caminho_setor / arquivo.name))
-                print(f"Sucesso: {nome} movido para {setor}")
-            else:
-                shutil.move(str(arquivo), str(pasta_invalidos / arquivo.name))
-                print(f"Arquivo inválido movido para INVALIDOS: {arquivo.name}")
-        except Exception as e:
-            print(f"Erro ao processar {arquivo.name}: {e}")
-
-    if contador == 0:
+'''Leitura'''
+def leitura_arquivos():
+    arquivos = list(pasta_entrada.rglob("*.pdf"))
+    if not arquivos:
         print("Nenhum termo foi encontrado na pasta de entrada.")
+    return arquivos
 
-def organizar_termos_si():
-    if not pasta_destino_si_raiz.exists():
-        pasta_destino_si_raiz.mkdir(parents=True, exist_ok=True)
-    if not pasta_invalidos_si.exists():
-        pasta_invalidos_si.mkdir(parents=True, exist_ok=True)
 
-    contador = 0
+'''Fragmentação do nome do arquivo'''
+def parse_nome(nome_completo):
+    partes = nome_completo.lower().split('-')
+    if len(partes) == 3:
+        nome, categoria, setor = partes
+        return {"nome": nome.strip(), "categoria": categoria.strip(), "setor": setor.strip()}
+    else:
+        return None
 
-    for arquivo in pasta_entrada.rglob("*.pdf"):
-        contador += 1
-        try:
-            nome_completo = arquivo.stem
-            partes = nome_completo.split('-')
 
-            if len(partes) == 3:
-                '''Categorias = VPN, Mídias Removíveis, Monitores, Periféricos, Notebooks'''
-                nome, categoria, setor = partes
-                caminho_setor = pasta_destino_ativos_raiz / setor
+'''Organização dos arquivos'''
+def mover_arquivo(arquivo, destino):
+    try:
+        shutil.move(str(arquivo), str(destino / arquivo.name))
+        print(f"Sucesso: {arquivo.name} movido para {destino}")
+    except Exception as e:
+        print(f"Erro ao mover {arquivo.name}: {e}")
+        shutil.move(str(arquivo), str(pasta_invalidos / arquivo.name))
+        print(f"{arquivo.name} movido para INVALIDOS devido ao erro.")
 
-                if not caminho_setor.exists():
-                    caminho_setor.mkdir(parents=True, exist_ok=True)
 
-                shutil.move(str(arquivo), str(caminho_setor / arquivo.name))
-                print(f"Sucesso: {nome} movido para {setor}")
+def organizar_termos():
+    arquivos  = leitura_arquivos()
+    SETORES_VALIDOS = ["Abastecimento", "Comercial", "Compras", "Controladoria", "CQ", "Custos", "Diretoria", "DP", "Engenharia", "Expedição", "Ferramentaria",
+    "Fiscal", "Inovação", "Logistica", "Manutenção", "Metrologia", "Moinho", "Novos Negocios", "PCP", "Produção", "Qualidade", "Recebimento", "RH", "TI", "Transporte",
+    "Tutilabs"]
+    
+    CATEGORIAS_EQUIPAMENTOS = ["Teclado", "Mouse", "Adaptador-USB", "Monitor", "Suporte-Notebook", "Fone-headset", "Pen-drive", "Fone-headfone", "Kit"]
+
+    CATEGORIAS_SI = ["Midias-removiveis", "VPN", "Smartphone-pessoal"]
+
+    for arquivo in arquivos:
+        resultado = parse_nome(arquivo.stem)
+        if resultado:
+            categoria = resultado["categoria"].capitalize()
+            setor = resultado["setor"].capitalize()
+
+            if categoria in CATEGORIAS_EQUIPAMENTOS and setor in SETORES_VALIDOS:
+                mover_arquivo(arquivo, pasta_destino_equipamentos)
+            elif categoria == "Notebook" and setor in SETORES_VALIDOS:
+                mover_arquivo(arquivo, pasta_destino_notebooks)
+            elif categoria in CATEGORIAS_SI and setor in SETORES_VALIDOS:
+                mover_arquivo(arquivo, pasta_destino_si)
             else:
-                shutil.move(str(arquivo), str(pasta_invalidos / arquivo.name))
-                print(f"Arquivo inválido movido para INVALIDOS: {arquivo.name}")
-        except Exception as e:
-            print(f"Erro ao processar {arquivo.name}: {e}")
-
-    if contador == 0:
-        print("Nenhum termo foi encontrado na pasta de entrada.")
+                print(f"Categoria ou setor inválido para {arquivo.name}. Movendo para INVALIDOS.")
+                mover_arquivo(arquivo, pasta_invalidos)
+        else:
+            print(f"Formato de nome inválido para {arquivo.name}. Movendo para INVALIDOS.")
+            mover_arquivo(arquivo, pasta_invalidos)
+    
+        
+                
